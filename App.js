c659b3f0db00d518a1344e682434052f91869527
cirogomess <ciro.ggj@gmail.com>
2020-06-12 23:38:09 -0300

    finished project

--- App.js
+++ App.js
@@ -3,10 +3,69 @@ import { StyleSheet, Text, View } from 'react-native';
 import Button from './src/components/Button'
 import Display from './src/components/Display'
 
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+}
+
 export default class App extends Component {
 
-  state = {
-    displayValue: '0'
+  state = { ...initialState }
+
+  addDigit = n => {
+
+
+
+    const clearDisplay = this.state.displayValue === '0'
+      || this.state.clearDisplay
+
+    if (n === '.' && !clearDisplay
+      && this.state.displayValue.includes('.')) {
+      return
+    }
+
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({ displayValue, clearDisplay: false })
+
+    if (n !== '.') {
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue
+      this.setState({ values })
+    }
+  }
+
+  clearMemory = () => {
+    this.setState({ ...initialState })
+  }
+
+  setOperation = operation => {
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true })
+    } else {
+      const equals = operation === '='
+      const values = [...this.state.values]
+      try {
+        values[0] =
+          eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      } catch (e) {
+        values[0] = this.state.values[0]
+      }
+
+      values[1] = 0
+      this.setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        //clearDisplay: !equals,
+        clearDisplay: true,
+        values,
+      })
+    }
   }
 
   render() {
@@ -14,23 +73,23 @@ export default class App extends Component {
       <View style={styles.container}>
         <Display value={this.state.displayValue} />
         <View style={styles.buttons}>
-          <Button label='AC' />
-          <Button label='/' />
-          <Button label='7' />
-          <Button label='8' />
-          <Button label='9' />
-          <Button label='*' />
-          <Button label='4' />
-          <Button label='5' />
-          <Button label='6' />
-          <Button label='-' />
-          <Button label='1' />
-          <Button label='2' />
-          <Button label='3' />
-          <Button label='+' />
-          <Button label='0' />
-          <Button label='.' />
-          <Button label='=' />
+          <Button label='AC' triple onClick={this.clearMemory} />
+          <Button label='/' operation onClick={() => this.setOperation('/')} />
+          <Button label='7' onClick={() => this.addDigit(7)} />
+          <Button label='8' onClick={() => this.addDigit(8)} />
+          <Button label='9' onClick={() => this.addDigit(9)} />
+          <Button label='*' operation onClick={() => this.setOperation('*')} />
+          <Button label='4' onClick={() => this.addDigit(4)} />
+          <Button label='5' onClick={() => this.addDigit(5)} />
+          <Button label='6' onClick={() => this.addDigit(6)} />
+          <Button label='-' operation onClick={() => this.setOperation('-')} />
+          <Button label='1' onClick={() => this.addDigit(1)} />
+          <Button label='2' onClick={() => this.addDigit(2)} />
+          <Button label='3' onClick={() => this.addDigit(3)} />
+          <Button label='+' operation onClick={() => this.setOperation('+')} />
+          <Button label='0' double onClick={() => this.addDigit(0)} />
+          <Button label='.' onClick={() => this.addDigit('.')} />
+          <Button label='=' operation onClick={() => this.setOperation('=')} />
         </View>
 
       </View>
--- src/components/Button.js
+++ src/components/Button.js
@@ -10,14 +10,30 @@ const styles = StyleSheet.create({
         backgroundColor: '#f0f0f0',
         textAlign: 'center',
         borderWidth: 1,
-        borderColor: '#888'
+        borderColor: '#888',
+        color: '#888'
+    },
+    operationButton: {
+        color: '#fff',
+        backgroundColor: '#fa8231'
+    },
+    buttonDouble: {
+        width: (Dimensions.get('window').width / 4) * 2
+    },
+    buttonTriple: {
+        width: (Dimensions.get('window').width / 4) * 3
     }
 })
 
 export default props => {
-    return (
-        <TouchableOpacity onPress={props.onClick}>
-            <Text style={styles.button}>{props.label}</Text>
-        </TouchableOpacity>
-    )
+
+    const styleButton = [styles.button]
+    if (props.double) styleButton.push(styles.buttonDouble)
+    if (props.triple) styleButton.push(styles.buttonTriple)
+    if (props.operation) styleButton.push(styles.operationButton)
+        return (
+            <TouchableOpacity onPress={props.onClick}>
+                <Text style={styleButton}>{props.label}</Text>
+            </TouchableOpacity>
+        )
 }
